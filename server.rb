98a7ae8e94433cf3e261126a2320d3fac4bdba64
ewamal <maluseckae@gmail.com>
2018-09-27 15:51:28 +0200

    add accept params

--- server.rb
+++ server.rb
@@ -1,5 +1,10 @@
 require 'bundler'; Bundler.require
 require 'JSON'
+require_relative "./lib/calculator"
+
+before do
+  @req_data = JSON.parse(request.body.read.to_s)
+end
 
 get '/hello-world' do
   "Hello World"
@@ -7,7 +12,26 @@ end
 
 get '/hello-world.json' do
   content_type :json
-
   {greeting: 'Hello World!'}.to_json
-
 end
+
+get '/average.json' do
+  content_type :json
+  calculator = Calculator.new(@req_data["array"])
+  {average: calculator.average}.to_json
+end
+
+get '/median.json' do
+  content_type(:json)
+
+  calculator = Calculator.new(@req_data["array"])
+  {median: calculator.median}.to_json
+end
+
+get '/mode.json' do
+  content_type(:json)
+  calculator = Calculator.new(@req_data["array"])
+  {mode: calculator.mode}.to_json
+end
+
+#curl -H 'Content-Type: application/json' -X POST --data '{"array":[1,2,3]}' http://localhost:4567/average.json
