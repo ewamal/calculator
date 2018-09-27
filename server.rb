1ff34f613dfd0bee7cf3ba84d73d0e1418452db8
ewamal <maluseckae@gmail.com>
2018-09-27 15:58:38 +0200

    clean-up :bento:

--- server.rb
+++ server.rb
@@ -6,32 +6,25 @@ before do
   @req_data = JSON.parse(request.body.read.to_s)
 end
 
-get '/hello-world' do
-  "Hello World"
-end
-
-get '/hello-world.json' do
-  content_type :json
-  {greeting: 'Hello World!'}.to_json
-end
-
-get '/average.json' do
-  content_type :json
-  calculator = Calculator.new(@req_data["array"])
-  {average: calculator.average}.to_json
-end
-
-get '/median.json' do
+get '/average' do
   content_type(:json)
 
   calculator = Calculator.new(@req_data["array"])
-  {median: calculator.median}.to_json
+  { average: calculator.average }.to_json
 end
 
-get '/mode.json' do
+get '/median' do
   content_type(:json)
+
   calculator = Calculator.new(@req_data["array"])
-  {mode: calculator.mode}.to_json
+  { median: calculator.median }.to_json
 end
 
-#curl -H 'Content-Type: application/json' -X POST --data '{"array":[1,2,3]}' http://localhost:4567/average.json
+get '/mode' do
+  content_type(:json)
+
+  calculator = Calculator.new(@req_data["array"])
+  { mode: calculator.mode }.to_json
+end
+
+#curl -H 'Content-Type: application/json' -X GET --data '{"array":[1,2,3]}' http://localhost:4567/average
